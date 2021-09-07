6ad96b0c4cf87f031e0993f118b418bbeb692161
vijendrabhadauria <58840300+vijendrabhadauria@users.noreply.github.com>
2021-09-08 01:52:37 +0530

    ITRF Doctors app

--- app/src/main/java/itrf/doctor/support/Const.java
+++ app/src/main/java/itrf/doctor/support/Const.java
@@ -2,7 +2,10 @@ package itrf.doctor.support;
 
 public class Const {
     public static String ServerUrl = "http://itrfhealthcare.ap-south-1.elasticbeanstalk.com/webresources/";
+    //    For Desktop
     //    public static String ServerUrl = "http://192.168.1.16:8080/ITRFHealthcare/webresources/";
+    //    For Laptop
+    //    public static String ServerUrl = "http://192.168.1.5:9090/ITRFHealthcare/webresources/";
     public static String doctorAppVersion = "16";
     public static String doctorAppVersion_Display = "1.6";
 }
