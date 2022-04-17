4ba11dff3dc6a3f58b56aa7da05a221b41143058
HP <mic_cs_bpl@yahoo.com>
2022-04-17 14:43:47 +0530

    Dynamic Server Config embedded

--- app/src/main/java/itrf/doctor/support/Const.java
+++ app/src/main/java/itrf/doctor/support/Const.java
@@ -1,13 +1,17 @@
 package itrf.doctor.support;
 
 public class Const {
-//    public static String ServerUrl = "http://itrftesthealthcare.ap-south-1.elasticbeanstalk.com/webresources/";
-    public static String ServerUrl = "https://itrf.in./webresources/";
-    //    public static String ServerUrl = "http://itrfhealthcare.ap-south-1.elasticbeanstalk.com/webresources/";
-    //    For Desktop
-    //    public static String ServerUrl = "http://192.168.1.16:8080/ITRFHealthcare/webresources/";
-    //    For Laptop
-    //    public static String ServerUrl = "http://192.168.1.5:9090/ITRFHealthcare/webresources/";
+    //  RunConfig - Possible values: production-env/test-env/localhost
+    RunConfig = "test-env",
+    HTTP_TEST_ENV_URL = "http://itrfhealthcaretest.ap-south-1.elasticbeanstalk.com/webresources/",
+    HTTP_LOCALHOST_URL = "http://192.168.1.5:8080/ITRFHealthcare/webresources/",
+    SSL_PROD_ENV_URL = "https://itrf.in/webresources/",
+    HTTP_PROD_ENV_URL = "http://itrfhealthcare.ap-south-1.elasticbeanstalk.com/webresources/",
+    ServerUrl = RunConfig.equalsIgnoreCase("test-env") ? HTTP_TEST_ENV_URL :
+            (RunConfig.equalsIgnoreCase("production-env") ? SSL_PROD_ENV_URL :
+            (RunConfig.equalsIgnoreCase("localhost") ? HTTP_LOCALHOST_URL :
+            "Invalid RunConfig value"));
+
     public static String doctorAppVersion = "16";
     public static String doctorAppVersion_Display = "1.6";
 }
